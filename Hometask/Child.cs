459aca3699154eae9948ceca95b137f87677c96c
User <User@WIN-8BH8LKBRQNO>
2023-11-29 00:05:48 +0500

    A new class named Child is created and given private fields and these fields are loaded with values through the constructor.

---
+++ Hometask/Child.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask
+{
+    public class Child
+    {
+        private string name;
+        private int age;
+
+        public Child(string _name, int _age)
+        {
+            _name = name;
+            _age = age;
+
+        }
+        public string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public void GettAllInfo()
+        {
+            Console.WriteLine($"Name: {name}\n" +
+                              $"Age: {age}");
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
